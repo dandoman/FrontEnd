3528ecd86616d9962d2a5668d96c6332f90dbb69
dinamix <tristano.tenaglia@mail.mcgill.ca>
2015-11-20 19:41:33 -0500

    this might not work for metrics and registration through node

---
+++ routes/register.js
@@ -0,0 +1,40 @@
+/**
+ * Created by dinamix on 11/20/15.
+ */
+"use strict";
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
+var springURI = "http://ec2-52-88-83-153.us-west-2.compute.amazonaws.com:8080/MetricsService/customer";
+//var metricURI = "http://ec2-52-88-83-153.us-west-2.compute.amazonaws.com:8080/MetricsService/metric/searchParams?param=benchpress&customerId=342efwdfwef&startTime=0";
+var postData = "";
+
+router.post('/', function (req, res, next) {
+    req.on('data', function (data) {
+        postData = JSON.parse(data);
+        //TODO make this post request be an async promise
+        //TODO maybe use a different library or promisify this itself
+        request.post({url: springURI, json: postData}, function (error, response, body) {
+            console.log('in request');
+            //TODO check for validated data or not and res.send appropriately
+            if (!error) {
+                //TODO make own redirection models
+                console.log("message is : " + response.body.message);
+                //TODO MAYBE SHOULD ONLY SEND BACK MESSAGE OR ERRORMESSAGE ON INVALID LOGINS
+                if (response.body.message === "Invalid email or password") {
+                    res.send({invalid: "invalid"});
+                } else {
+                    console.log('not in error' + JSON.stringify(response.body));
+                    res.send(response.body);
+                }
+            } else {
+                console.log('in error');
+                console.log('in error ' + response.statusCode);
+                console.log(error);
+            }
+        });
+    });
+});
+
+module.exports = router
